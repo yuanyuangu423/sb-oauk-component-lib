c8537e72cf6c581958c2d846651c0eabb903506e
wendy <yuanyuan.gu@openapply.com>
2024-11-27 16:20:02 +0800

    add index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+// index.js
+export { default as Button } from './src/components/Foundation/Button/Button.vue';
+export { default as Header } from './src/components/Foundation/Header/Header.vue';
+export { default as Page } from './src/components/Foundation/Page/index';
